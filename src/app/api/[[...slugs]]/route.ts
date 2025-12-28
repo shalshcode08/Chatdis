19dc7a9f925c7a06eaa266862ca9d796c7a8674d
shalshcode08 <somushrestha448@gmail.com>
2025-12-28 18:28:15 +0530

    added auth, middleware, message endpoint, roomId verification

--- src/app/api/[[...slugs]]/route.ts
+++ src/app/api/[[...slugs]]/route.ts
@@ -1,10 +1,45 @@
+import { redis } from "@/lib/redis";
 import { Elysia } from "elysia";
+import { nanoid } from "nanoid";
+import { z } from "zod";
+import { authMiddleware } from "./auth";
 
-const rooms = new Elysia({ prefix: "/room" }).post("/", () => {
-  console.log("CREATED A NEW ROOMS");
+const ROOM_TTL_SECONDS = 60 * 10;
+
+const rooms = new Elysia({ prefix: "/room" }).post("/", async () => {
+  const roomId = nanoid();
+
+  await redis.hset(`meta:${roomId}`, {
+    connected: [],
+    createdAt: Date.now(),
+  });
+
+  await redis.expire(`meta:${roomId}`, ROOM_TTL_SECONDS);
+
+  return { roomId };
 });
 
-const app = new Elysia({ prefix: "/api" }).use(rooms);
+const messsages = new Elysia({ prefix: "/messages" }).use(authMiddleware).post(
+  "/",
+  async ({ body, auth }) => {
+    const { sender, text } = body;
+    const { roomId } = auth;
+
+    const roomExists = await redis.exists(`meta.${roomId}`);
+    if (!roomExists) {
+      throw new Error("Room does not exist");
+    }
+  },
+  {
+    query: z.object({ roomId: z.string() }),
+    body: z.object({
+      sender: z.string().max(100),
+      text: z.string().max(1000),
+    }),
+  }
+);
+
+const app = new Elysia({ prefix: "/api" }).use(rooms).use(messsages);
 
 export const GET = app.fetch;
 export const POST = app.fetch;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import { client } from "@/lib/client";
 import { useMutation } from "@tanstack/react-query";
 import { nanoid } from "nanoid";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 const ANIMALS = ["wolf", "hawk", "bear", "shark"];
@@ -15,6 +16,7 @@ const generateUsername = () => {
 
 export default function Home() {
   const [username, setUsername] = useState("");
+  const router = useRouter();
 
   useEffect(() => {
     const main = () => {
@@ -36,6 +38,10 @@ export default function Home() {
   const { mutate: createRoom } = useMutation({
     mutationFn: async () => {
       const res = await client.room.post();
+
+      if (res.status === 200) {
+        router.push(`/room/${res.data?.roomId}`)
+      }
     },
   });
 
