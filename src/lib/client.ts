2449da40111bfcb87d110569c1f3438c33b313d8
shalshcode08 <somushrestha448@gmail.com>
2025-12-29 15:03:13 +0530

    fix: changed api routes

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,5 +1,10 @@
 import { treaty } from "@elysiajs/eden";
 import type { App } from "@/app/api/[[...slugs]]/route";
 
-// .api to enter /api prefix
-export const client = treaty<App>("localhost:3000").api;
+const apiBaseUrl =
+  process.env.NEXT_PUBLIC_API_URL ??
+  (typeof window !== "undefined"
+    ? window.location.origin
+    : "http://localhost:3000");
+
+export const client = treaty<App>(apiBaseUrl).api;
